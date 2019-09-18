673e724ee286ea909c2104051f28b88db3d258c1
gunawan26 <33386634+gunawan26@users.noreply.github.com>
2019-09-18 12:21:15 +0800

    coba vue

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Auth::routes();
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/{any}', 'SinglePageController@index')->where('any', '.*');
